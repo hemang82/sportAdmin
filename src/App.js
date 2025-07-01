11cb84e237bd3f33193d17f7998e4b38a4ff5805
hemang82 <109712867+hemang82@users.noreply.github.com>
2025-07-01 19:06:36 +0530

    Change code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from './Router/index';
-import DisableSelectionStyle from "./component/DisabledBrowser/DisableSelectionStyle";
+// import DisableSelectionStyle from "./component/DisabledBrowser/DisableSelectionStyle";
 // import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 // import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
 
 function App() {
   return (
     <>
-      <DisableSelectionStyle disable={true} />
+      {/* <DisableSelectionStyle disable={true} /> */}
       <BrowserRouter basename="/">
         <Router />
       </BrowserRouter>
